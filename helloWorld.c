69cc32ff47a39561980a035706fd68d63861e2f1
jjh4591 <48662456+jjh4591@users.noreply.github.com>
2019-03-18 16:51:26 +0900

    Update helloWorld.c

--- helloWorld.c
+++ helloWorld.c
@@ -2,6 +2,12 @@
 #include stdlib.h
 
 int main(){
+    char ch;
     printf("Hello, world!");
+    printf("문자입력 : ")
+    scanf("%c", &ch);
+
+    
+    printf("입력된 문자 : \"%c\"입니다", ch);
     return 0;
 }
